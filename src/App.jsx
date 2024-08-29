f87b0257d4cf9ead376658e50767179dec6cdc5f
Lucas <velvettouch1004@gmail.com>
2024-08-29 15:39:54 -0500

    todo using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Todolist from "./components/todolist";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Todolist />
-    </>
+    </Provider>
   );
 }
 
