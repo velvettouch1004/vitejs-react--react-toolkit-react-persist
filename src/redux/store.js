56e2ee8ed34b991752e31e204e5a65b7723b4a5d
Lucas <velvettouch1004@gmail.com>
2024-08-30 10:33:04 -0500

    completed

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
 const persistConfig = {
-  key: "root",
+  key: "roots",
   storage,
 };
 
